a7f28855d588807ce55ecdf2f89f7af8e6e2ae89
Guilherme Marcelino <guimoliveira01@outlook.com>
2024-07-13 16:22:22 -0300

    Use of fetch API
    - Faça com que as transições sejam assíncronas através de: ajax, fetch e promises: troca de XMLHttpRequest para fetch, uma API mais moderna.

--- script.js
+++ script.js
@@ -13,14 +13,13 @@ function generateProxyURL(destination) {
 }
 
 function request(url, callback) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            callback(this.responseText);
+    fetch(url, {method: 'GET'}).then((response) => {
+        if (response.ok) {
+            response.text().then((text) => {
+                callback(text);
+            });
         }
-    };
-    xhttp.open("GET", url);
-    xhttp.send();
+    });
 }
 
 function fetchGitHubRepos(callback) {
